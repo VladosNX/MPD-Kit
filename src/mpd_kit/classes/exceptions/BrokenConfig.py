8929fdc92862c071f75d30981b243baa64426531
VladosNX <vladosnx@yandex.ru>
2026-03-05 22:03:57 +0700

    Added configparse.parser.parse_plain function, added BrokenConfig exception

---
+++ src/mpd_kit/classes/exceptions/BrokenConfig.py
@@ -0,0 +1,1 @@
+class BrokenConfig(Exception): pass
--- src/mpd_kit/corelibs/configparse/parser.py
+++ src/mpd_kit/corelibs/configparse/parser.py
@@ -2,17 +2,10 @@ import os
 import sys
 from mpd_kit.classes.Configuration import Configuration
 from mpd_kit.classes.exceptions.UnknownValue import UnknownValue
+from mpd_kit.classes.exceptions.BrokenConfig import BrokenConfig
 from mpd_kit.classes.exceptions.NoConfigFile import NoConfigFile
 
-def parse_config(project='.'):
-    dotmpd_file = f'{project}/DOTMPD'
-    if not os.path.exists(dotmpd_file):
-        raise NoConfigFile()
-
-    file = open(dotmpd_file, mode='r', encoding='utf-8')
-    content = file.read()
-    file.close()
-
+def parse_plain(content):
     lines = content.split('\n')
     if len(lines) < 1 or lines[0] != 'DOTMPD1':
         raise NoConfigFile()
@@ -23,8 +16,23 @@ def parse_config(project='.'):
         if line == '': continue
         if line[0] == '#': continue
 
-        key, value = line.split('>')
+        try:
+            key, value = line.split('>')
+        except ValueError:
+            raise BrokenConfig()
         result.setValue(key, value)
 
     result.validateRequired()
     return result
+
+def parse_config(project='.'):
+    dotmpd_file = f'{project}/DOTMPD'
+    if not os.path.exists(dotmpd_file):
+        raise NoConfigFile()
+
+    file = open(dotmpd_file, mode='r', encoding='utf-8')
+    content = file.read()
+    file.close()
+
+    return parse_plain(content)
+
